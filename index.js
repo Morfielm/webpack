fc975b554c2a5dd1bf39c0bb5e5af2d2b26627ea
Mariia Dvoriankina <morfielmm@gmail.com>
2023-05-21 21:02:40 +0200

    lesson 18 task 4

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import './index.css';
+import Icon from './assets/javascript.jpg';
+
 console.log('Hello World!');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'I love JavaScript';
+document.body.append(h1);
+
+const myIcon = new Image();
+myIcon.src = Icon;
+document.body.append(myIcon);
+
+// https://github.com/Morfielm/webpack zadanie 3
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,44 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: path.resolve(__dirname, 'index.js'),
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-    clean: true
-  }
+    mode: "development",
+    entry: path.resolve(__dirname, 'index.js'),
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, 'index.html'),
+    })],
+    devServer: {
+        static: './dist',
+        port: 8080,
+        open: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(?:js|mjs|cjs)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            ['@babel/preset-env', { targets: "defaults" }]
+                        ]
+                    }
+                }
+            }
+        ],
+    },
 };
